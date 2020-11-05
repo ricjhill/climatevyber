e9f73a4b780a093a2f639d907a8bfa95a34fed85
Richard Hill <ricjhill@protonmail.com>
2020-11-05 23:34:34 +0100

    Working out pages its ugy ASF
    Signed-off-by: Richard Hill <ricjhill@protonmail.com>

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-export default () => (
+
+
+const   PostTemplate = ({data: post}) => (
 <Layout>
-<h2> Programmatically created blog post! </h2>
+/*<div>
+<h1>{post.markdownRemark.frontmatter.title}</h1>
+<div dangerouslySetInnerHTML={{  __html: post.markdownRemark.html  }} />
+</div>*/
 </Layout>
-
 )
+
+
+export const query = graphql`
+query($slug: String!){
+  markdownRemark(fields: {
+    slug: {eq: $slug } })
+   {
+    html
+    frontmatter {
+      title
+    }
+  }
+ }
+`
+
+export default PostTemplate
